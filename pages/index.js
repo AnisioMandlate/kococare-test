4cef3983bc015eab234baeedb5ffc51d97ea1908
AnisioMandlate <anisiomandlate@gmail.com>
2021-10-16 09:23:05 +0530

    style: Fix spacing issue on homepage

--- pages/index.js
+++ pages/index.js
@@ -9,27 +9,29 @@ export default function Home() {
   const [phone, setPhone] = useState("");
   return (
     <div className={styles.container}>
-      <header>
-        <Image src="/logo.png" alt="Kococare Logo" width={32} height={31.2} />
-      </header>
+      <div>
+        <header>
+          <Image src="/logo.png" alt="Kococare Logo" width={32} height={31.2} />
+        </header>
 
-      <h1 className={styles.title}>
-        Hello <br /> Parent!
-      </h1>
-      <p className={styles.text}>
-        Thank you for choosing care. It’s super awesome to have you here. Let’s
-        start your parenting journey.
-      </p>
-      <h2 className={styles.subtitle}>
-        Let’s set up your account.
-        <br />
-        You can do so with your phone number.
-      </h2>
-      <PhoneInput
-        country="in"
-        value={phone}
-        onChange={(phone) => setPhone(phone)}
-      />
+        <h1 className={styles.title}>
+          Hello <br /> Parent!
+        </h1>
+        <p className={styles.text}>
+          Thank you for choosing care. It’s super awesome to have you here.
+          Let’s start your parenting journey.
+        </p>
+        <h2 className={styles.subtitle}>
+          Let’s set up your account.
+          <br />
+          You can do so with your phone number.
+        </h2>
+        <PhoneInput
+          country="in"
+          value={phone}
+          onChange={(phone) => setPhone(phone)}
+        />
+      </div>
 
       <button className={styles.button}>
         <Link href="/otp">Send OTP to verify</Link>
