531af65d777a70b8720c3c22390c4ec79ea9a45c
AnisioMandlate <anisiomandlate@gmail.com>
2021-10-16 07:44:34 +0530

    feat: Add homepage and modify global styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import styles from "@styles/Home.module.css";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello There. This is Kococare</h1>
+    <div className={styles.container}>
+      <header>
+        <Image src="/logo.png" alt="Kococare Logo" width={32} height={31.2} />
+      </header>
+
+      <h1>Hello Parent!</h1>
+      <p>
+        Thank you for choosing care. It’s super awesome to have you here. Let’s
+        start your parenting journey
+      </p>
+      <h2>
+        Let’s set up your account.
+        <br />
+        You can do so with your phone number.
+      </h2>
     </div>
   );
 }
